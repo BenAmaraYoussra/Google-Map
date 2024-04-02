4438efe7d2254f6c42dc170284e53dcc09af23bf
BenAmaraYoussra <benamarayousra3@gmail.com>
2024-04-03 00:09:23 +0200

    first commit

---
+++ googlemap-react-demo-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MapContainer from './map';//import du composant
+
+
+function App() {
+  return (
+    <div className="App">
+ <h1>Google Maps :</h1> 
+
+    <MapContainer/> 
+      </div> 
+
+  );
+}
+
+export default App;
